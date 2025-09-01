3eb96d0594a8e1821372cecd51c9f0b81baf0358
ADITYA CHAURASIYA <153356096+aditychaurasiya@users.noreply.github.com>
2025-09-01 23:29:13 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (33.21%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,12 +1,19 @@
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         length = len(nums)
+#         for i in range(length):
+#             if i not in nums:
+#                 return i
+#                 break
+#         else:
+#             return length
+
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
-        for i in range(length):
-            if i not in nums:
-                return i
-                break
-        else:
-            return length
-                
+    def missingNumber(self,  nums: List[int]) -> int:
+        n = len(nums)               
+        totalSum = sum(nums)
+        expSum = n * (n + 1) // 2
+        return expSum - totalSum
+
 
         
